8de8ce1d41b9d06afda40dfbedf84ebf69ee32c4
ayushanand308 <ayushanand_mc21a11_34@dtu.ac.in>
2024-04-04 20:46:34 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,12 @@ const http=require('http');
 const server=http.createServer(app);
 const {Server}=require('socket.io');
 const io=new Server(server);
-const cors = require('cors');
-const path = require('path');
-
-app.use(cors());
-
 
 
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+// Route for serving the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
