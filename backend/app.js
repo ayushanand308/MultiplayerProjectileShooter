9145533898360a44641e8c79b82149b8274a2eb6
ayushanand308 <ayushanand_mc21a11_34@dtu.ac.in>
2024-04-04 18:43:40 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,10 @@ const http=require('http');
 const server=http.createServer(app);
 const {Server}=require('socket.io');
 const io=new Server(server);
+const cors = require('cors');
 const path = require('path');
+
+app.use(cors());
 
 
 
